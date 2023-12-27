0b78bafd424e01499cf38221547e0f28ba18c0c1
MahdiMoosavi <3535bahr2089@gmial.com>
2023-12-28 00:02:29 +0330

    adding first time

---
+++ Content/javascript/Modal.js
@@ -0,0 +1,85 @@
+function showModal(pid) {
+  /* scrolling with transition: */
+  window.scrollTo({
+    top: 100,
+    behavior: "smooth",
+  });
+
+  ShowingModal.className = "showingModal_Search";
+
+  /* for entering Madal from StoreSection SlideShow */
+  StoreSection.style.height = "80px";
+  ShortCutsTitles.style.display = "none";
+  otherholder.style.display = "none";
+  /*  */
+
+  setTimeout(() => {
+      DetailHolderSearching.style.display = "none";
+      searchingBackground.style.position = "absolute";
+  }, 500);
+
+  searchingBackground.style.transition = "1s";
+  searchingBackground.style.opacity = 0;
+
+  curEL = GamesData.find((el) => {
+    return el.numberOfgame == pid;
+  });
+
+  AddCartBT.setAttribute("data-id", curEL.numberOfgame);
+  AddWishListBT.setAttribute("data-id", curEL.numberOfgame);
+
+    Modal_BigPic.src = "Content/Media/Store_recommended/gallery/" + curEL.GameImages[1];
+    Modal_Pic1.src = "Content/Media/Store_recommended/gallery/" + curEL.GameImages[1];
+    Modal_Pic2.src = "Content/Media/Store_recommended/gallery/" + curEL.GameImages[2];
+    Modal_Pic3.src = "Content/Media/Store_recommended/gallery/" + curEL.GameImages[3];
+    Modal_Pic4.src = "Content/Media/Store_recommended/gallery/" + curEL.GameImages[0];
+
+    onvanPic.src = "Content/Media/Store_recommended/gallery/" + curEL.GameImages[0];
+
+  Modal_GameName.innerHTML = curEL.GameName;
+
+  AddCartBT.innerHTML = 'Add To Cart "' + curEL.PriceWithDiscount + '€"';
+  AddWishListBT.innerHTML = "Add To WishList";
+}
+
+function CloseModalFunction() {
+  searchingBackground.style.position = "relative";
+
+  ShowingModal.className = "hidingModal_Search";
+
+  DetailHolderSearching.style.display = "flex";
+
+  searchingBackground.style.transition = "1s";
+  searchingBackground.style.opacity = 1;
+  searchingBackground.style.visibility = "visible";
+
+  if (EnteringFromSore_Slider == true) {
+    EnteringFromSore_Slider = false;
+    Going_StoreSection();
+
+    window.scrollTo({
+      top: 500,
+      behavior: "smooth",
+    });
+  }
+}
+
+Modal_Pic1.onclick = function () {
+  Modal_BigPic.src = Modal_Pic1.src;
+};
+
+Modal_Pic2.onclick = function () {
+  Modal_BigPic.src = Modal_Pic2.src;
+};
+
+Modal_Pic3.onclick = function () {
+  Modal_BigPic.src = Modal_Pic3.src;
+};
+
+Modal_Pic4.onclick = function () {
+  Modal_BigPic.src = Modal_Pic4.src;
+};
+
+closeModal.onclick = function () {
+  CloseModalFunction();
+};
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Steam_API_.Global" Language="C#" %>
